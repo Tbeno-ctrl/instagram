9bfa2b7a9cb65db2a296b519a6e3fbdbe580264c
Tbeno-ctrl <84532920+Tbeno-ctrl@users.noreply.github.com>
2021-12-08 20:21:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "909774147:AAFN3ypDAG7csapqYKAoctTQNWVtkOnTBJU"
 
 def download(bot, update):
     message = update.effective_message
